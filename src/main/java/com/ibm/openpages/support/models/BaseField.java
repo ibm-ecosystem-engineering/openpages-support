dc6f3620d897ac6799fb57774f989129975d0646
Sean Sundberg <seansund@us.ibm.com>
2024-06-02 09:04:05 -0500

    Fixes fieldgroup bug (#4)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ src/main/java/com/ibm/openpages/support/models/BaseField.java
@@ -0,0 +1,78 @@
+package com.ibm.openpages.support.models;
+
+import com.ibm.openpages.support.util.FieldType;
+
+public abstract class BaseField<T> implements FieldMetadata, ResultLabel  {
+    final protected String originalValue;
+    final protected String value;
+    final protected String label;
+    final protected FieldType<?> fieldType;
+
+    protected BaseField(FieldGroup fieldGroup, String value, FieldType<?> fieldType) {
+        this(fieldGroup, value, value, fieldType);
+    }
+
+    protected BaseField(FieldGroup fieldGroup, String value, String label, FieldType<?> fieldType) {
+        this.originalValue = value;
+        this.value = fieldGroup != null ? fieldGroup.fieldName(value) : value;
+        this.label = label;
+        this.fieldType = fieldType;
+    }
+
+    @Override
+    public boolean isEnumField() {
+        return fieldType.isEnumField();
+    }
+
+    @Override
+    public boolean isStringField() {
+        return fieldType.isStringField();
+    }
+
+    @Override
+    public boolean isIntegerField() {
+        return fieldType.isIntegerField();
+    }
+
+    @Override
+    public boolean isDecimalField() {
+        return fieldType.isDecimalField();
+    }
+
+    @Override
+    public boolean isDateField() {
+        return fieldType.isDateField();
+    }
+
+    @Override
+    public boolean isUserField() {
+        return fieldType.isUserField();
+    }
+
+    @Override
+    public boolean isBooleanField() {
+        return fieldType.isBooleanField();
+    }
+
+    @Override
+    public boolean isCurrencyField() {
+        return fieldType.isCurrencyField();
+    }
+
+    @Override
+    public FieldType<?> fieldType() {
+        return fieldType;
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public String label() {
+        return label;
+    }
+
+    abstract protected T withFieldGroup(FieldGroup fieldGroup);
+}
--- src/main/java/com/ibm/openpages/support/models/Field.java
+++ src/main/java/com/ibm/openpages/support/models/Field.java
@@ -2,79 +2,21 @@ package com.ibm.openpages.support.models;
 
 import com.ibm.openpages.support.util.FieldType;
 
-public class Field implements FieldMetadata, ResultLabel {
-    private FieldGroup fieldGroup;
-    final private String value;
-    final private String label;
-    final private FieldType<?> fieldType;
+public class Field extends BaseField<Field> {
 
-    protected Field(FieldGroup fieldGroup, String fieldName, FieldType<?> fieldType) {
-        this(fieldGroup, fieldName, fieldName, fieldType);
+    protected Field(FieldGroup fieldGroup, String value, FieldType<?> fieldType) {
+        super(fieldGroup, value, value, fieldType);
     }
 
     protected Field(FieldGroup fieldGroup, String value, String label, FieldType<?> fieldType) {
-        this.fieldGroup = fieldGroup;
-        this.value = fieldGroup.fieldName(value);
-        this.label = label;
-        this.fieldType = fieldType;
+        super(fieldGroup, value, label, fieldType);
     }
 
-    @Override
-    public boolean isEnumField() {
-        return fieldType.isEnumField();
+    protected Field withFieldGroup(FieldGroup fieldGroup) {
+        return new Field(fieldGroup, this.originalValue, this.label, this.fieldType);
     }
 
-    @Override
-    public boolean isStringField() {
-        return fieldType.isStringField();
-    }
-
-    @Override
-    public boolean isIntegerField() {
-        return fieldType.isIntegerField();
-    }
-
-    @Override
-    public boolean isDecimalField() {
-        return fieldType.isDecimalField();
-    }
-
-    @Override
-    public boolean isDateField() {
-        return fieldType.isDateField();
-    }
-
-    @Override
-    public boolean isUserField() {
-        return fieldType.isUserField();
-    }
-
-    @Override
-    public boolean isBooleanField() {
-        return fieldType.isBooleanField();
-    }
-
-    @Override
-    public boolean isCurrencyField() {
-        return fieldType.isCurrencyField();
-    }
-
-    @Override
-    public FieldType fieldType() {
-        return fieldType;
-    }
-
-    @Override
-    public String value() {
-        return value;
-    }
-
-    @Override
-    public String label() {
-        return label;
-    }
-
-    protected void setFieldGroup(FieldGroup fieldGroup) {
-        this.fieldGroup = fieldGroup;
+    public String toString() {
+        return "[Field: value=" + value + ", label=" + label + ", fieldType=" + fieldType + "]";
     }
 }
