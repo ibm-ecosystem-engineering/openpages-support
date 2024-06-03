580a2921adef2dc79d2be1502bc24df704822180
Sean Sundberg <seansund@us.ibm.com>
2024-06-03 00:46:20 -0500

    Update logic to build query clause
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/main/java/com/ibm/openpages/support/util/query/Operation.java
+++ src/main/java/com/ibm/openpages/support/util/query/Operation.java
@@ -1,15 +1,35 @@
 package com.ibm.openpages.support.util.query;
 
+import com.ibm.openpages.support.models.IObjectType;
+import com.ibm.openpages.support.models.ResultValue;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+
 public enum Operation {
-    EQUAL("=");
+    EQUALS("="),
+    IN("IN", inValue -> (inValue instanceof List<?>) ? inValue : Collections.singletonList(inValue));
 
     private final String value;
+    private final Function<Object, Object> valueFormatter;
 
     Operation(String value) {
+        this(value, in -> in);
+    }
+
+    Operation(String value, Function<Object, Object> valueFormatter) {
         this.value = value;
+        this.valueFormatter = valueFormatter;
     }
 
     public String value() {
         return value;
+    }
+
+    public String toClause(IObjectType type, ResultValue field, Object inValue) {
+        final Object value = valueFormatter.apply(inValue);
+
+        return "[" + type.name() + "].[" + field.baseValue() + "] " + this.value() + " " + value;
     }
 }
